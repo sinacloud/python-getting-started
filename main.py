997a409da11af63347b1725a3a9fc42304400a37
chenfeng2 <chenfeng2@staff.sina.com.cn>
2016-01-04 13:50:07 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.write("Hello, world!")
+
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(os.environ['PORT'])
+    tornado.ioloop.IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
